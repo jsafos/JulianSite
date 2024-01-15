2c8cc47fd134e7f01c39d8d16d69486da6ee20e8
jsafos <safosj1@mcmaster.ca>
2024-01-14 21:05:54 -0500

    Added nav bar and drop down list

---
+++ js/main.js
@@ -0,0 +1,80 @@
+let topButton = document.getElementById("to-top-button")
+
+//show button once user scrolls past 20px from top of document
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
+
+function navClickOpen(){
+    let buttonCode = document.querySelector(".nav-bar2");
+    buttonCode.innerHTML = `
+    
+        <ul class="nav-bar-list">
+
+        <button class="js-nav-button nav-x fa-solid fa-x" onclick="navClickClose()"></button>
+
+        <li class="active">
+            <a href="#intro" >
+                <span class="nav-bar-option"> Home </span>
+            </a>
+        </li>
+
+                        
+        <li class="active">
+            <a href="#about" >
+                <span class="nav-bar-option"> About </span>
+            </a>
+        </li>
+
+                        
+        <li class="active">
+            <a href="#resume" >
+                <span class="nav-bar-option"> Resume </span>
+            </a>
+        </li>
+
+
+                        
+        <li class="active">
+            <a href="#projects" >
+                <span class="nav-bar-option"> Portfolio </span>
+            </a>
+        </li>
+
+                        
+        <li class="active">
+            <a href="#footer" >
+                <span class="nav-bar-option"> Contact </span>
+            </a>
+        </li>
+
+        </ul>    
+    `;
+
+    console.log("CLicked Hamburger")
+}
+
+
+function navClickClose(){
+    let buttonCode = document.querySelector(".nav-bar2");
+    buttonCode.innerHTML = `<button class="js-nav-button nav-hambuger fa-solid fa-bars" onclick="navClickOpen()"></button>
+    `;
+
+}
+
+
+
+
+
